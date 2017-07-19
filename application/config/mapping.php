373dd0852a0d451fb202804d34a7f11d70b91033
Vinicius Fernando <vihh.fernando@gmail.com>
2017-07-19 10:12:17 -0300

    obter treinamentos na api

--- application/config/mapping.php
+++ application/config/mapping.php
@@ -130,4 +130,11 @@ $config['Venda'] = [
     'loja'          => 'Loja'
 ];
 
+$config['Treinamento'] = [
+    'nome'      => 'Nome',
+    'descricao' => 'Descricao',
+    'foto'      => 'Foto',
+    'video'     => 'Video'
+];
+
 /* end of file */
