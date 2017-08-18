5a7e73298025cff2fa9f7b3ee2abb9bcd1aad20f
Vinicius Fernando <vihh.fernando@gmail.com>
2017-08-18 19:12:50 -0300

    adiciona join left

--- application/finders/FuncionariosFinder.php
+++ application/finders/FuncionariosFinder.php
@@ -56,7 +56,7 @@ class FuncionariosFinder extends MY_Model {
         $this->db->from( $this->table.' f' )
         ->select( 'CodFuncionario as Ações, f.CodFuncionario as Codigo, l.Nome as Loja, f.CPF, f.RG, f.Nome, f.Pontos, f.Celular, f.UID, f.Token, f.Cargo, f.Email,
         f.Endereco, f.Numero, f.Complemento, f.Cep, c.Nome as Cidade, e.UF as Estado')
-        ->join( 'Lojas l', 'l.CodLoja = f.CodLoja' )
+        ->join( 'Lojas l', 'l.CodLoja = f.CodLoja', 'left' )
         ->join( 'Cidades c', 'c.CodCidade = f.CodCidade', 'left' )
         ->join( 'Estados e', 'e.CodEstado = f.CodEstado', 'left' );
         return $this;
@@ -72,7 +72,7 @@ class FuncionariosFinder extends MY_Model {
         $this->db->from( $this->table.' f' )
         ->select( 'f.CodFuncionario as Codigo, l.Nome as Loja, f.UID, f.Token, f.Cargo, f.CPF, f.RG, f.Nome, f.Email, f.Pontos,
         f.Endereco, f.Numero, f.Complemento, f.Cep, f.Celular, c.Nome as Cidade, e.UF as Estado')
-        ->join( 'Lojas l', 'l.CodLoja = f.CodLoja' )
+        ->join( 'Lojas l', 'l.CodLoja = f.CodLoja', 'left' )
         ->join( 'Cidades c', 'c.CodCidade = f.CodCidade', 'left' )
         ->join( 'Estados e', 'e.CodEstado = f.CodEstado', 'left' );
         return $this;
