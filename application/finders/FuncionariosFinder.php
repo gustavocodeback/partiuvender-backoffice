6abc61cc25f7a12945fb6ce7982b6bac388bf308
Vinicius Fernando <vihh.fernando@gmail.com>
2017-08-23 11:47:35 -0300

    correcao finder func

--- application/finders/FuncionariosFinder.php
+++ application/finders/FuncionariosFinder.php
@@ -72,7 +72,7 @@ class FuncionariosFinder extends MY_Model {
     public function exportar() {
         $this->db->from( $this->table.' f' )
         ->select( 'f.CodFuncionario as Codigo, f.NeoCode, l.Nome as Loja, f.UID, f.Token, f.Cargo,
-        f.CPF, f.RG, f.Nome, f.Email, f.Pontos, sum(v.Pontos) as PontosVendas, sum(q.Pontos) as PontosQuiz
+        f.CPF, f.RG, f.Nome, f.Email, f.Pontos,
         f.Endereco, f.Numero, f.Complemento, f.Cep, f.Celular, c.Nome as Cidade, e.UF as Estado')
         ->join( 'Lojas l', 'l.CodLoja = f.CodLoja', 'left' )
         ->join( 'Cidades c', 'c.CodCidade = f.CodCidade', 'left' )
